555b67031cd4db671ddb1fc1c7160606b3559304
D7mnCh <da7manch0@gmail.com>
2026-06-26 22:36:49 +0100

    feat: finish impl bumb allocator

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Building arenas (region-based) memorey allocators in rust, implementing bumb, st
 # Concepts
 ## general purpose allocator
 it is the simpliest allocator, it just allocates or deallactes memory, no fancy methods or algorithms, in c or rust, it is the default allocator
-## Bumb/leaner allocator
-- it basically can allocate memory leanery only in one direction, that's means you can't like deallocate last allocation.
+## Bumb/leaner allocator (need re-write)
+- it basically can allocate memory leanery only in one direction, that's means you can't like deallocate prev allocation.
 > [!NOTE]
 > if you want an allocator that deallocate memory on the last allocation on the arena, consider reading the next allocator (the stack allocator)
 - if you want deallocation, you'll need to deallocate all the region/arena all in once (that's where the name "bumb" came from!)
@@ -21,6 +21,16 @@ it is the simpliest allocator, it just allocates or deallactes memory, no fancy 
 
 # What i learned
 
+- each block in memroy represent an address, we deal with addresses as bytes, and hexdecimal number
 - rust std returend `u8` as a pointee type, cuz it's easier to deal with pointers arithmetics (only in pure bytes: u8 == 1 byte!)
-- `memory alignment` referes to how data are arranged in memory
-- `alignemnt` refers to num of how many bytes/blocks that a size/chunk should have so the last one will get full fill.If the size % alignment == 0, 
+- a `layout` is a description/request of a chunk of memory(no allocation yet), that request a size and an alignment
+    - the required alignment is just an information, you'll impl the offset of the pointer manually to make an aligned data, the length is need for allocation
+- `memory alignment` describes where piece of data should store in memory, a data is properly aligned if its "starting address" is a "multiple" of its "required alignemnt", when this condition satisfied, we say "the data is "aligned" or "properley aligned""
+    - required alignment = is a num that describe where to allocate a data in memory in order to be valid, that valid address most be divisble by that number(required alignment)
+    - example usage: "data have alignment of 8 bytes(a valid address for this data are divible by 8)"
+- each type have different alignment (i thought why just make them all have alignment 1, so you can place any data in any address. But that will dencrease CPU efficiency as i know(need search))
+- struct alignment is largetst field alignment ((why?, need search))
+- The size of a value is always a multiple of its alignment
+
+- `padding` is an ignored address(empty) within the arena (that was being allocated), it contains a garbage value or if initialize 0 value
+    - less padding = more performance(need search)
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 #![allow(dead_code)]
-// NOTE i don't know how to use that arena -.-
 use std::{
     alloc::{alloc_zeroed, dealloc, Layout},
     ptr,
 };
 
+const DATA_ALIGNMENT: usize = 1;
+const LENGTH: usize = 1000;
+
 struct BumbAlloc {
     start: *mut u8,
     tracker: *mut u8,
-    offset: usize,
-    // the layout is our region/arena
+    used_bytes: usize,
     layout: Layout,
 }
 
@@ -17,35 +18,43 @@ impl BumbAlloc {
     // we need "const generic" cuz an array will be build with a provided
     //length array type must be know at compile time
     fn build_with(length: usize) -> Self {
-        let length = length / size_of::<u64>();
-        let alignment = 64;
-
-        let layout = Layout::from_size_align(length, alignment).unwrap();
+        let layout = Layout::from_size_align(length, DATA_ALIGNMENT).unwrap();
+        println!(
+            "[INFO] requested {} bytes of memory to allocate",
+            layout.size()
+        );
         let ptr = unsafe { alloc_zeroed(layout) };
 
         Self {
             tracker: ptr,
             start: ptr,
-            offset: 0,
+            used_bytes: 0,
             layout,
         }
     }
 
-    fn append_with(&mut self, requested_bytes: usize) -> Option<*mut u8> {
-        if self.layout.size() >= self.offset + requested_bytes {
-            //todo!("give that pointer an initialized data");
-            let ptr = Some(self.tracker);
-            self.offset += requested_bytes;
+    fn append_with(&mut self, layout: &Layout) -> *mut u8 {
+        if self.layout.size() >= self.used_bytes + layout.align() {
+            println!("[INFO] requesting {} bytes to allocate", layout.size());
+            // return this pointer to current tracker
+            let ptr = self.tracker;
+            // updating arena tracker pointer
+            let to_be_aligned = align_requested_data(self.used_bytes, &layout);
+            println!("[INFO] offsetting track pointer by {to_be_aligned}");
             unsafe {
-                self.tracker = self.tracker.add(self.offset);
+                // offset the pointer(tracker) to get an align address
+                self.tracker = self.tracker.add(to_be_aligned);
             }
 
-            let remaining = self.layout.size().saturating_sub(self.offset);
-            println!("space remaining for allocation: {remaining}");
+            // updating arena used bytes
+            println!("[INFO] add {to_be_aligned} bytes to arena's used bytes");
+            self.used_bytes += align_requested_data(self.used_bytes, &layout);
+
+            let remaining = self.layout.size().saturating_sub(self.used_bytes);
+            println!("[INFO] space remaining for allocation: {remaining}");
             return ptr;
         }
-
-        eprintln!(
+        panic!(
             "[ERROR] arena is full :
                  arena provided length               = {length}
                  space remaining for more allocation = {remaining}
@@ -53,15 +62,13 @@ impl BumbAlloc {
             length = self.layout.size(),
             remaining = 0
         );
-
-        None
     }
 
     // TODO
     pub fn reallocate_with_new(&mut self, _layout: Layout) {}
 
     fn uninitialize(&mut self) {
-        self.offset = 0;
+        self.used_bytes = 0;
         self.layout = Layout::new::<()>();
         self.start = ptr::null_mut();
     }
@@ -74,7 +81,6 @@ impl BumbAlloc {
 
 #[test]
 fn arena_size() {
-    const LENGTH: usize = 1000;
     let arena = BumbAlloc::build_with(LENGTH);
 
     assert_eq!(arena.layout.size(), LENGTH);
@@ -82,42 +88,134 @@ fn arena_size() {
 
 #[test]
 fn appending_arena() {
-    const LENGTH: usize = 1000;
+    const LENGTH: usize = 10;
     let mut arena = BumbAlloc::build_with(LENGTH);
 
-    let num_bytes = 500;
-    arena.append_with(num_bytes);
+    let i32_layout = Layout::new::<i32>();
+    arena.append_with(&i32_layout);
+    arena.append_with(&i32_layout);
+    let i16_layout = Layout::new::<i16>();
+    arena.append_with(&i16_layout);
 
-    assert_eq!(arena.offset, num_bytes);
-
-    arena.append_with(num_bytes);
-    // error: can't allocate new memoroy, arena.size < arena.offset
-    //arena.append_with(num_bytes);
+    assert_eq!(arena.used_bytes, LENGTH);
 }
 
 #[test]
 fn bumbing_arena() {
-    const LENGTH: usize = 548;
     let mut arena = BumbAlloc::build_with(LENGTH);
 
-    let num_bytes = 500;
-    arena.append_with(num_bytes);
+    let layout = Layout::new::<i32>();
+    arena.append_with(&layout);
 
     arena.bumb();
-    assert!(arena.offset != num_bytes);
+
+    assert!(arena.used_bytes != layout.align());
     assert!(arena.layout.size() == 0);
 }
 
 #[test]
-fn append_rust_data_to_arena() {
-    // NOTE who to allocate that inside my allocator ?
-    let array = ['1', '2', '3'];
-
-    const LENGTH: usize = 1000;
+fn append_primitives_to_arena() {
     let mut arena = BumbAlloc::build_with(LENGTH);
-    if let Some(ptr) = arena.append_with(size_of_val(&array)) {
-        //unsafe { ptr.write }
+
+    let layout = Layout::new::<[i16; 2]>();
+    let ptr_1 = arena.append_with(&layout).cast::<[i16; 2]>();
+    assert_eq!(arena.used_bytes, 4);
+
+    let layout = Layout::new::<[i32; 3]>();
+    let ptr_2 = arena.append_with(&layout).cast::<[i32; 3]>();
+    let padding = 4;
+    let prev_area_used_bytes = 4;
+    assert_eq!(
+        arena.used_bytes,
+        prev_area_used_bytes + layout.size() + padding
+    );
+
+    // testing arena.tracker
+    unsafe {
+        ptr_1.write([1, 2]);
+        ptr_2.write([1000, 3324, 1231]);
+        assert!(ptr_1.read() == [1, 2]);
+        assert!(ptr_2.read() == [1000, 3324, 1231]);
+    }
+}
+
+#[test]
+fn append_structs_to_arena() {
+    #[derive(Debug)]
+    struct Foo {
+        data: bool,
+        more_data: u64,
+    }
+    #[derive(Debug)]
+    struct Boo {
+        some_data: u8,
+        more_more_data: char,
     }
 
-    assert_eq!(arena.offset, 8);
+    let foo_layout = Layout::new::<Foo>();
+    let boo_layout = Layout::new::<Boo>();
+    let mut arena = BumbAlloc::build_with(LENGTH);
+
+    let ptr_1 = arena.append_with(&foo_layout).cast::<Foo>();
+    assert_eq!(arena.used_bytes, foo_layout.size());
+
+    let ptr_2 = arena.append_with(&boo_layout).cast::<Boo>();
+    let prev_area_used_bytes = 16;
+    assert_eq!(arena.used_bytes, prev_area_used_bytes + boo_layout.size());
+
+    unsafe {
+        ptr_1.write(Foo {
+            data: true,
+            more_data: 2837,
+        });
+
+        ptr_2.write(Boo {
+            some_data: 255,
+            more_more_data: 'h',
+        });
+
+        dbg!(ptr_1);
+        dbg!(ptr_1.read());
+        dbg!(ptr_2);
+        dbg!(ptr_2.read());
+    }
 }
+
+fn align_requested_data(current_used_bytes: usize, layout: &Layout) -> usize {
+    let bytes_to_add = if layout.size() != layout.align() {
+        let bytes_to_align_reqstd_data =
+            if current_used_bytes == 0 || current_used_bytes % layout.align() == 0 {
+                0
+            } else if current_used_bytes > layout.align() {
+                current_used_bytes.div_ceil(layout.align())
+            } else {
+                layout.align() - current_used_bytes
+            };
+        bytes_to_align_reqstd_data + layout.size()
+    } else {
+        if current_used_bytes >= layout.align() {
+            let offset = layout.align();
+            offset
+        } else {
+            let offset = layout.align() - current_used_bytes;
+            offset
+        }
+    };
+    bytes_to_add
+}
+
+//fn align_requested_data(current_bytes_used: usize, layout: &Layout) -> usize {
+//if data_alignment % DATA_ALIGNMENT == 0 {
+//    return data_alignment;
+//}
+//else if DATA_ALIGNMENT < data_alignment {
+//    println!("[WARNING] requested layout alignment is greater then arena alignemnt");
+//    let multiplier = data_alignment.div_ceil(DATA_ALIGNMENT);
+//    return DATA_ALIGNMENT * multiplier;
+//} else if DATA_ALIGNMENT > data_alignment {
+//    println!("[WARNING] requested layout alignment is less then arena alignemnt");
+//    return DATA_ALIGNMENT;
+//} else {
+//    return 0;
+//}
+//}
